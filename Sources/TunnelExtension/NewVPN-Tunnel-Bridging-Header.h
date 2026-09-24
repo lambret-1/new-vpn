65f51f60be20097577bbd099d4b836e14d9f4478
lambret-1 <lambret-1@users.noreply.github.com>
2026-09-24 17:40:32 +0800

    fix: 添加OC-Swift桥接头文件

---
+++ Sources/TunnelExtension/NewVPN-Tunnel-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  NewVPN-Tunnel-Bridging-Header.h
+//  NewVPN-Tunnel
+//
+//  隧道扩展 OC-Swift 桥接头
+//
+
+#ifndef NewVPN_Tunnel_Bridging_Header_h
+#define NewVPN_Tunnel_Bridging_Header_h
+
+#import "Libbox平台接口OC.h"
+
+#endif

c90d40dfc80443b313ecc8d5a4bffca9001d7ef7
lambret-1 <lambret-1@users.noreply.github.com>
2026-09-25 02:39:52 +0800

    fix: 平台接口补全underNetworkExtension等关键方法，修复LibboxNewService异常崩溃

--- Sources/TunnelExtension/Libbox平台接口OC.m
+++ Sources/TunnelExtension/Libbox平台接口OC.m
@@ -10,32 +10,35 @@
 
 @implementation Libbox平台接口OC
 
-// 暂时注释掉 underNetworkExtension，使用默认值，排查是否导致 LibboxNewService 崩溃
-// - (BOOL)underNetworkExtension {
-//     return YES;
-// }
+/// 当前运行在 Network Extension 中，必须返回 YES
+- (BOOL)underNetworkExtension {
+    return YES;
+}
+
+/// VPN 包含所有网络流量
+- (BOOL)includeAllNetworks {
+    return YES;
+}
+
+/// iOS 没有 procfs，返回 NO
+- (BOOL)useProcFS {
+    return NO;
+}
+
+/// 不使用平台自动检测接口控制
+- (BOOL)usePlatformAutoDetectInterfaceControl {
+    return NO;
+}
+
+/// 清空 DNS 缓存（iOS 由系统管理，空实现）
+- (void)clearDNSCache {
+}
 
 - (void)writeLog:(NSString * _Nullable)message {
     if (message && self.日志回调) {
         self.日志回调(2, message);
     }
 }
-
-// 暂时注释掉以下方法，使用默认值，排查是否导致 LibboxNewService 崩溃
-// - (BOOL)includeAllNetworks {
-//     return YES;
-// }
-//
-// - (BOOL)useProcFS {
-//     return NO;
-// }
-//
-// - (BOOL)usePlatformAutoDetectControl {
-//     return NO;
-// }
-//
-// - (void)clearDNSCache {
-// }
 
 /// 重写 openTun，返回 packetFlow 的文件描述符
 /// 这是 sing-box 内核能读写系统 VPN 数据包的关键

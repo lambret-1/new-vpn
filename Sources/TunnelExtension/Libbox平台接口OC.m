a7eb4df1fd918077e9a0bd96f047d37b4f190c82
lambret-1 <lambret-1@users.noreply.github.com>
2026-09-25 00:37:03 +0800

    fix: 运行时枚举NEPacketTunnelFlow所有属性方法找真实fd

--- Sources/TunnelExtension/Libbox平台接口OC.m
+++ Sources/TunnelExtension/Libbox平台接口OC.m
@@ -6,6 +6,7 @@
 //
 
 #import "Libbox平台接口OC.h"
+#import <objc/runtime.h>
 
 @implementation Libbox平台接口OC
 
@@ -53,56 +54,82 @@
 
 + (int32_t)安全获取文件描述符:(id)packetFlow error:(NSError **)error {
     NSMutableString *调试信息 = [NSMutableString string];
+    Class 类 = [packetFlow class];
 
-    // 尝试多种属性名获取文件描述符
-    NSArray *属性列表 = @[@"fileDescriptor", @"socket", @"_fileDescriptor", @"_socket", @"fileHandle", @"_fileHandle"];
-
-    for (NSString *属性名 in 属性列表) {
-        @try {
-            id 值 = [packetFlow valueForKey:属性名];
-            if ([值 isKindOfClass:[NSNumber class]]) {
-                int32_t fd = [(NSNumber *)值 intValue];
-                [调试信息 appendFormat:@"%@=%d; ", 属性名, fd];
-                // 只接受大于 2 的文件描述符（0/1/2 是 stdin/stdout/stderr）
-                if (fd > 2) {
-                    return fd;
-                }
-            } else if (值 != nil) {
-                [调试信息 appendFormat:@"%@=%@(非数字); ", 属性名, 值];
-            } else {
-                [调试信息 appendFormat:@"%@=nil; ", 属性名];
-            }
-        } @catch (NSException *异常) {
-            [调试信息 appendFormat:@"%@=异常(%@); ", 属性名, 异常.name];
-        }
+    // 枚举所有属性（包括父类）
+    [调试信息 appendFormat:@"类名=%@; ", NSStringFromClass(类)];
+    unsigned int 属性数量 = 0;
+    objc_property_t *属性列表 = class_copyPropertyList(类, &属性数量);
+    NSMutableArray *属性名列表 = [NSMutableArray array];
+    for (unsigned int i = 0; i < 属性数量; i++) {
+        const char *属性名 = property_getName(属性列表[i]);
+        NSString *名字 = [NSString stringWithUTF8String:属性名];
+        [属性名列表 addObject:名字];
     }
+    free(属性列表);
+    [调试信息 appendFormat:@"属性列表=%@; ", 属性名列表];
 
-    // 尝试 performSelector 方式
-    NSArray *选择子列表 = @[@"fileDescriptor", @"socket", @"fileDescriptor"];
-    for (NSString *选择子名 in 选择子列表) {
-        SEL 选择子 = NSSelectorFromString(选择子名);
-        if ([packetFlow respondsToSelector:选择子]) {
+    // 枚举所有方法
+    unsigned int 方法数量 = 0;
+    Method *方法列表 = class_copyMethodList(类, &方法数量);
+    NSMutableArray *方法名列表 = [NSMutableArray array];
+    for (unsigned int i = 0; i < 方法数量; i++) {
+        SEL 方法名 = method_getName(方法列表[i]);
+        [方法名列表 addObject:NSStringFromSelector(方法名)];
+    }
+    free(方法列表);
+    [调试信息 appendFormat:@"方法列表=%@; ", 方法名列表];
+
+    // 尝试所有属性中包含 fd/socket/file/desc 的
+    for (NSString *属性名 in 属性名列表) {
+        NSString *小写名 = [属性名 lowercaseString];
+        if ([小写名 containsString:@"fd"] || [小写名 containsString:@"socket"] ||
+            [小写名 containsString:@"file"] || [小写名 containsString:@"desc"] ||
+            [小写名 containsString:@"tun"] || [小写名 containsString:@"interface"]) {
             @try {
-                int (*函数指针)(id, SEL) = (int (*)(id, SEL))[packetFlow methodForSelector:选择子];
-                int fd = 函数指针(packetFlow, 选择子);
-                [调试信息 appendFormat:@"SEL %@=%d; ", 选择子名, fd];
-                if (fd > 2) {
-                    return fd;
+                id 值 = [packetFlow valueForKey:属性名];
+                if ([值 isKindOfClass:[NSNumber class]]) {
+                    int32_t fd = [(NSNumber *)值 intValue];
+                    [调试信息 appendFormat:@"%@=%d; ", 属性名, fd];
+                    if (fd > 2) {
+                        if (error) *error = nil;
+                        return fd;
+                    }
+                } else {
+                    [调试信息 appendFormat:@"%@=%@; ", 属性名, 值];
                 }
             } @catch (NSException *异常) {
-                [调试信息 appendFormat:@"SEL %@=异常; ", 选择子名];
+                [调试信息 appendFormat:@"%@=异常; ", 属性名];
             }
-        } else {
-            [调试信息 appendFormat:@"SEL %@=无响应; ", 选择子名];
         }
     }
 
-    // 记录 packetFlow 的类名和所有属性
-    [调试信息 appendFormat:@"类名=%@; ", NSStringFromClass([packetFlow class])];
+    // 尝试所有方法中包含 fd/socket/file/desc 的
+    for (NSString *方法名 in 方法名列表) {
+        NSString *小写名 = [方法名 lowercaseString];
+        if (([小写名 containsString:@"fd"] || [小写名 containsString:@"socket"] ||
+             [小写名 containsString:@"file"] || [小写名 containsString:@"desc"]) &&
+            ![方法名 containsString:@":"]) {
+            SEL 选择子 = NSSelectorFromString(方法名);
+            if ([packetFlow respondsToSelector:选择子]) {
+                @try {
+                    int (*函数指针)(id, SEL) = (int (*)(id, SEL))[packetFlow methodForSelector:选择子];
+                    int fd = 函数指针(packetFlow, 选择子);
+                    [调试信息 appendFormat:@"方法%@=%d; ", 方法名, fd];
+                    if (fd > 2) {
+                        if (error) *error = nil;
+                        return fd;
+                    }
+                } @catch (NSException *异常) {
+                    [调试信息 appendFormat:@"方法%@=异常; ", 方法名];
+                }
+            }
+        }
+    }
 
     if (error) {
         *error = [NSError errorWithDomain:@"com.newvpn.tunnel" code:-2
-                                 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"未找到有效 TUN 文件描述符。调试信息：%@", 调试信息]}];
+                                 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"未找到有效 TUN 文件描述符。%@", 调试信息]}];
     }
     return -1;
 }
